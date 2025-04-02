004b55bf5938c3055e1ae4ab2c282b8ad38699d3
anchalnalwale <nalwaleanchal@gmail.com>
2025-04-02 21:42:04 +0530

    Updated files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import './App.css'
 import Product from './Product'
+import { list } from 'postcss';
 
 function App() {
   let options = ["hi-tech" , "durable"];
-  let options2 = {a:"hi-tech" , b:"durable"};
+  
   return (
     <div>
-      <Product title="Laptop" price={30000} features={options} features2={options2}/>
+      <Product title="Laptop" price={40000} features={options}/>
     </div>
   )
 }
--- src/Product.jsx
+++ src/Product.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 
 const Product = ({title,price,features,features2}) => {
-  console.log(features);
-  console.log(features2);
   return (
-    <div className='bg-emerald-300 h-24 w-48 items-center justify-center rounded'>
+    <div className='bg-emerald-300 h-32 w-48 items-center justify-center rounded'>
         <h1 className='pt-5 text-2xl'>{title}</h1>
-        <h3>{price}</h3>
-        <p>{features}</p>
-        <p>{features2.b}</p>
+        <h3>Price : {price}</h3>
+        {price>30000 && <p>Discount of 5%</p>}
+        {price>30000 ? <p>Discount of 5%</p>:null}
     </div>
   )
 }
