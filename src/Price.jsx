7c1bc711d940d85c46815309aa91a2bdee5a2542
anchalnalwale <nalwaleanchal@gmail.com>
2025-06-16 22:14:08 +0530

    Done changes

--- src/Price.jsx
+++ src/Price.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
 
 const Price = ({oldPrices,newPrices}) => {
-  // let oldStyles = {
-  //   TextDecorationLine : "line-through",
-  // };
-  // let newStyles = {
-  //   fontWeight : "bold",
-  // };
   return (
-    <div className='justify-center'>
+    <div className='flex flex-col justify-center'>
         <span className='line-through'>{oldPrices}</span>
-        &nbsp;&nbsp;&nbsp;
         <span className='font-bold'>{newPrices}</span>
     </div>
   )
--- src/Product.jsx
+++ src/Product.jsx
@@ -11,9 +11,9 @@ const Product = ({title ,idx}) => {
     ["Wireless"],
   ];
   return (
-    <div className='bg-emerald-500 mx-5 my-5 pt-5 pl-5 rounded-lg items-center'>
-      <h1 className='text-2xl'>{title}</h1>
-      <p>{description[idx]}</p>
+    <div className='flex flex-col bg-emerald-500 mx-5 my-5 pt-5 pl-5 rounded-lg justify-center items-center'>
+      <h1 className='text-2xl p-5'>{title}</h1>
+      <p >{description[idx]}</p>
       <Price oldPrices={oldPrices[idx]} newPrices={newPrices[idx]}/>
     </div>
   )
